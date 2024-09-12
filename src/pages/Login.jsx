d4b1cf6ceae58e3899089b90db557333877c0ea1
Sehee Jang <seheejang94@gmail.com>
2024-09-12 16:51:08 +0900

    add: 로그인, 회원가입 페이지 기본 레이아웃

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1>로그인</h1>
+      <p>
+        계정이 없으신가요? <Link to="signup">회원가입</Link>
+      </p>
+    </div>
+  );
 };
 
 export default Login;
