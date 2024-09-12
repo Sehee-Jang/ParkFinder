7c24cafeae280f23a71e2d35624f5ccfe85c9c95
RYC0208 <ryc0208@naver.com>
2024-09-13 00:31:20 +0900

    add: 임시로 apu, mutate js 생성

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import BookMark from "../components/BookMark";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <BookMark />
+      Home
+    </div>
+  );
 };
 
 export default Home;
