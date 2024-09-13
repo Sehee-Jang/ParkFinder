207f9e7a224d5fb2b8a42bdc848eb41cc843ed5b
Sehee Jang <seheejang94@gmail.com>
2024-09-13 19:06:32 +0900

    add: 로그인/로그아웃 로직 추가

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { useState } from "react";
 
-const AuthForm = () => {
+const AuthForm = ({ mode, onSubmit }) => {
   const [formData, setFormData] = useState({
     id: "",
     password: "",
     nickname: mode === "signup" ? "" : "" // 회원가입일 때만 닉네임 필드 활성화, 빈 문자열로 초기화
   });
-  const handleChange = () => {};
-  const handleSubmit = () => {};
+
+  // onChange할 때마다 formData의 값이 업데이트 되야 함
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value // id가 변경되면 id가 인식, password가 변겨오디면 password가 인식 됨
+    });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //로그인 페이지에서 이미 완성된 handleLogin으로 onSubmit으로 줄 로직을 내려보내주고 있기때문에 onSubmit으로 온 것을 그대로 실행만 시켜주면 됨
+    onSubmit(formData);
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -23,7 +35,7 @@ const AuthForm = () => {
       />
       {/* 회원가입일 경우에만 닉네임 입력 필드 표시 */}
       {mode === "signup" && (
-        <Input
+        <input
           type="text"
           name="nickname"
           value={formData.nickname}
