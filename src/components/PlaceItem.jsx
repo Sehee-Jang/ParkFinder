1bc212e0183789f49d628c9d9927fb2d31b4a716
RYC0208 <ryc0208@naver.com>
2024-09-18 07:14:23 +0900

    feat:  CSS 수정, 북마크 버튼 컴포넌트화

--- src/components/PlaceItem.jsx
+++ src/components/PlaceItem.jsx
@@ -1,33 +1,34 @@
 import React from "react";
 import { useHandleBookMark } from "../hooks/bookmarkMutate";
+import BookmarkButton from "./BookmarkButton";
 
 const PlaceItem = ({ place, USER_ID }) => {
-  //place에 좋아요를 누른 회원들의 ID값에 현재 로그인된 사용자의 ID 값이 하나라도 있는지 확인하기
+  // place에 좋아요를 누른 회원들의 ID값에 현재 로그인된 사용자의 ID 값이 하나라도 있는지 확인하기
   const bookmarked = place.bookmarks.some((user) => user.userId === USER_ID);
-
+ 
   const mutatePatchBookMark = useHandleBookMark();
 
-  return (
-    <div key={place.id} className="flex flex-col items-center gap-[5px] border border">
-      <div className="text-base font-bold">{place.title}</div>
-      <div className="text-sm">즐겨찾기 갯수: {place.bookmarks.length}</div>
-      <button
-        onClick={() =>
-          mutatePatchBookMark({
-            id: place.id,
-            userId: USER_ID,
-            bookmarked
-          })
-        }
-        className={`px-4 py-2 rounded text-white font-semibold focus:outline-none w-[150px] ${
-          bookmarked ? "bg-yellow-500" : "bg-gray-500"
+  const handleBookmarkClick = () => {
+    mutatePatchBookMark({
+      id: place.id,
+      userId: USER_ID,
+      bookmarked
+    });
+  };
 
-          // transition-colors: 색상 변경에 애니메이션 효과를 추가
-          // duration-300: 애니메이션의 지속 시간을 300밀리초로 설정
-        } transition-colors duration-300`}
-      >
-        {bookmarked ? "즐겨찾기 취소" : "즐겨찾기"}
-      </button>
+  return (
+    <div
+      key={place.id}
+      className="flex flex-col items-center justify-between w-1/6 h-[230px] gap-[5px] rounded-[8px] border border"
+    >
+      <div className="h-3/5 bg-teal-100 w-full rounded-t-lg"></div>
+      <div className="flex flex-col w-full h-2/5 p-1.5">
+        <div className="flex flex-row items-center w-full justify-between">
+          <div className="text-[16px] font-bold">{place.title}</div>
+          <BookmarkButton bookmarked={bookmarked} onClick={handleBookmarkClick} />
+        </div>
+        <div className="text-gray-500 text-[14px]">어쩌구 저쩌구 이런저런 얘기</div>
+      </div>
     </div>
   );
 };
