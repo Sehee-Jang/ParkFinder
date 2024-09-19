d2f158237f6a940fbeaade3a0c7733e5375dcfcd
ayoung <jo.ayoung.1210@gmail.com>
2024-09-19 10:57:47 +0900

    merge: mypage / fix: router

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,7 @@
-import React, { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Layout from "./components/Layout";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import MyPage from "./pages/MyPage";
-import ProtectedRoute from "./components/ProtectedRoute";
-import BookmarkTestHome from "./pages/bookmarkTest/bookmarkTestHome";
-import BookmarkTestMyPage from "./pages/bookmarkTest/bookmarkTestMyPage";
+import Router from "./shared/Router";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route
-            path="/bookmarkhome"
-            element={
-              <ProtectedRoute>
-                <BookmarkTestHome />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/bookmarkmypage"
-            element={
-              <ProtectedRoute>
-                <BookmarkTestMyPage />
-              </ProtectedRoute>
-            }
-          />
-        </Route>
-        <Route
-          path="/mypage"
-          element={
-            <ProtectedRoute>
-              <MyPage />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Router />;
 };
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import KakaoMap from "../components/KakaoMap";
-// import BookMark from "../components/BookMark";
 
 const Home = () => {
   return (
     <>
       <KakaoMap />
-      {/* <BookMark /> */}
     </>
   );
 };
