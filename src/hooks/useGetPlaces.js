69eb65cc6e15bacea344ba565e8c2e74d2c49d9b
RYC0208 <ryc0208@naver.com>
2024-09-20 17:04:07 +0900

    feat: 로그아웃 수정

--- src/hooks/useGetPlaces.js
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import bookmarkApi from "../api/bookmark";
-
-export const useGetPlaces = () => {
-  return useQuery({
-    queryKey: ["places"],
-    queryFn: bookmarkApi.fetchPlaces
-  });
-};
